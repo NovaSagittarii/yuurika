19da9e860e19ad53e5e990805cd3199dbaa3cf50
NoviceProgramming <thomas1210@gmail.com>
2018-06-13 22:51:11 -0700

    client-server communication

--- index.js
+++ index.js
@@ -5,25 +5,109 @@ const io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
+const config = {
+  width:  400,
+  height: 400,
+  broadcastInterval: false, //ms
+  targetFrameRate: 60, //fps
+};
+const keys = "wasdjk".split('');
+const stats = {
+  accel: 1,
+  ts: Math.PI*0.003
+};
+let keySet = {};
+for(let i = 0; i < keys.length; i ++){
+  keySet[keys[i]] = false;
+}
+
 var active = 0;
+var plyr = {};
+var plyrID = [];
+function Player(){
+  this.x = plyrID.length*50;
+  this.y = 0;
+  this.a = 0;  // rotation alignment
+  this.xv = 0;
+  this.yv = 0;
+  this.av = 0; // angular vel
+  this.ap = 100; // armour
+  this.sp = 100; // shield
+  this.state = Object.assign(keySet);
+}
+Player.prototype.update = function(){
+  if(this.state.w || this.state.s){
+    this.xv += Math.cos(this.a) * stats.accel * (1-2*this.state.s);
+    this.yv += Math.sin(this.a) * stats.accel * (1-2*this.state.s);
+  }
+  if(this.state.a || this.state.d) this.av += stats.ts * (1-2*this.state.a);
+  this.x += this.xv;
+  this.y += this.yv;
+  this.a += this.av;
+  this.xv /= 1.1;
+  this.yv /= 1.1;
+  this.av /= 1.1;
+  if(this.x < 0) this.x = 0;
+  if(this.y < 0) this.y = 0;
+  if(this.x > config.width) this.x = config.width;
+  if(this.y > config.height) this.y = config.height;
+}
+Player.prototype.updateState = function(input){
+  if(isNaN(this.x) || isNaN(this.y) || isNaN(this.a)) return true; // I N V A L I D    M O V E M E N T   :hyperAngery:
+  //for(let i = 0; i < keys.length; i ++) this.state[keys[i]] = input[keys[i]];
+  this.state = Object.assign(input);
+}
+
+var broadcast = true;
+function update(){
+  for(let i = 0; i < plyrID.length; i ++){
+    let socketID = plyrID[i];
+    let self = plyr[socketID];
+    self.update();
+    // Checking from index of all tokenids, call from object and update.
+    if(broadcast || !config.broadcastInterval){
+      let data = {self: Object.assign(self), plyrs: []};
+      for(let j = 0; j < plyrID.length; j ++){
+        if(i === j) continue;
+        data.plyrs.push(Object.assign(plyr[plyrID[j]]));
+      }
+      io.to(socketID).emit('update', data);
+    }
+  }
+  broadcast = false;
+}
+function broadcastData(){
+  broadcast = true;
+}
 
 io.on('connection', function(socket){
   console.log(' > new connection! cID: ' + socket.id);
-  setTimeout(function(){socket.broadcast.emit('nP', ++active);}, 1000);
+  setTimeout(function(){socket.broadcast.emit('nP', ++active);}, 2000);
+  plyr[socket.id] = new Player();
+  plyrID.push(socket.id);
 
-  socket.on('keydown', function(msg){
-    console.log('keydown ' + msg);
+  socket.on('input', function(data){
+    if(plyr[socket.id].updateState(data)){
+      console.log("DISCONNECTED!!!")
+      socket.disconnect();
+    }
   });
-  socket.on('mD', mouseMsg);
-  function mouseMsg(data){
-    socket.broadcast.emit('mD', data);
-  }
+  socket.on('requestConfig', function(){
+    io.to(socket.id).emit('setConfig', config);
+  })
   socket.on('disconnect', function(){
+    delete plyr[socket.id];
+    plyrID.splice(plyrID.indexOf(socket.id), 1);
     console.log(' < disconnection!  cID: ' + socket.id);
     socket.broadcast.emit('nP', --active);
   });
 });
 
 http.listen(3000, '0.0.0.0', function(){
-  console.log('listening on *:3000');
+  console.log(`listening on *:3000\nRunning at ${(1e3/Math.round(1e3 / config.targetFrameRate)).toFixed(2)}FPS`);
 });
+setInterval(update, Math.round(1e3 / config.targetFrameRate));
+if(config.broadcastInterval) setInterval(broadcastData, config.broadcastInterval);
+
+
+//
--- public/render.js
+++ public/render.js
@@ -1,46 +1,132 @@
+/*
+pushMatrix() / popMatrix() -> push() / pop()
+mouse {} -> touch {}
+*/
+
+var config = {};
 var socket;
 var aU = 0;
+const sendable = "wasdjk".split('');
+
+//Player vars
+var x = 0;
+var y = 0;
+var a = 0;
+var xv = 0;
+var yv = 0;
+var av = 0;
+var ap = 100;
+var sp = 100;
+var plyrs = [];
+
+var accel = 1;
+var ts = Math.PI * 0.003;
+var state;
+
+var keys = [];
 
 function updateUsers(active) {
   aU = active;
 }
 function update(data){
-  if(data.senderID === socket.id) return;
-  stroke(data.r*1.2, data.g*1.2, data.b*1.2);
-  strokeWeight(4);
-  line(data.x, data.y, data.px, data.py);
+  x = data.self.x;
+  y = data.self.y;
+  a = data.self.a;
+  xv = data.self.xv;
+  yv = data.self.yv;
+  av = data.self.av;
+  ap = data.self.ap;
+  sp = data.self.sp;
+  state = data.self.state;
+  plyrs = data.plyrs;
+  console.log("updated! to " + state)
 }
-
-function draw() {
-
-    noStroke();
-    fill(255, 255, 255);
-    rect(0, 0, 700, 50);
-    fill(0, 0, 0);
-    text(aU + " active users", 5, 25);
+function updateConfig(newConfig){
+  config = Object.assign(newConfig);
 }
 
 function setup() {
-  createCanvas(1600, 1600);
+  createCanvas(600, 600);
   background(255, 255, 255);
-  socket = io.connect('http://47.147.17.164:3000/');
-  socket.on('mD', update);
+  socket = io.connect('http://47.147.17.164:3000');
+  socket.on('update', update);
   socket.on('nP', updateUsers);
+  socket.on('setConfig', updateConfig);
+  socket.emit('requestConfig', "");
   updateUsers();
+  for(var i = 0; i < sendable.length; i ++) keys[sendable[i]] = false;
 }
-function mouseDragged() {
-  var data = {
-    x: mouseX,
-    y: mouseY,
-    px: pmouseX,
-    py: pmouseY,
-    senderID: socket.id,
-    r: socket.id.charCodeAt(0) + socket.id.charCodeAt(1),
-    g: socket.id.charCodeAt(2) + socket.id.charCodeAt(3),
-    b: socket.id.charCodeAt(4) + socket.id.charCodeAt(5)
+function draw() {
+  background(0, 0, 0);
+  strokeWeight(2);
+  noStroke();
+  fill(255, 255, 255);
+  //rect(0, 0, 700, 50);
+  //fill(0, 0, 0);
+  text(plyrs.length+1 + " active users", 5, 25);
+
+  translate(300, 300);
+  rotate(3/2*Math.PI - a + av);
+  translate(-x, -y);
+
+  fill(255, 150);
+  for(var i = 0; i < plyrs.length; i ++){
+    push();
+    translate(plyrs[i].x, plyrs[i].y);
+    rotate(plyrs[i].a);
+    triangle(10, 0, -10, -7, -10, 7);
+    pop();
+  }
+
+  stroke(255, 255, 255);
+  noFill();
+  rect(0, 0, config.width, config.height);
+
+  translate(x, y);
+  rotate(a);
+
+  fill(255, 255, 255);
+  triangle(10, 0, -10, -7, -10, 7);
+  resetMatrix();
+
+
+
+
+  if(keys.w || keys.s){
+    xv += Math.cos(a) * accel * (1-2*keys.s);
+    yv += Math.sin(a) * accel * (1-2*keys.s);
+  }
+  if(keys.a || keys.d) av += ts * (1-2*keys.a);
+  x += xv;
+  y += yv;
+  a += av;
+  xv /= 1.1;
+  yv /= 1.1;
+  av /= 1.1;
+}
+
+var currKey = null;
+function keyPressed() {
+  keys[key.toLowerCase()] = true;
+  let data = currKey = {
+    w: keys.w,
+    s: keys.s,
+    a: keys.a,
+    d: keys.d,
+    pw: keys.j,
+    sw: keys.k
   };
-  socket.emit('mD', data);
-  stroke(socket.id.charCodeAt(0) + socket.id.charCodeAt(1), socket.id.charCodeAt(3) + socket.id.charCodeAt(2), socket.id.charCodeAt(5) + socket.id.charCodeAt(4));
-  strokeWeight(10);
-  line(mouseX, mouseY, pmouseX, pmouseY)
+  socket.emit('input', data);
+}
+function keyReleased() {
+  keys[key.toLowerCase()] = false;
+  let data = currKey = {
+    w: keys.w,
+    s: keys.s,
+    a: keys.a,
+    d: keys.d,
+    pw: keys.j,
+    sw: keys.k
+  };
+  socket.emit('input', data);
 }
