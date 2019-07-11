6adcbf0c266f33e74e4bd1f34284b381f1d6bd2a
NoviceProgramming <thomas1210@gmail.com>
2019-07-11 13:47:19 -0700

    metadata & landing page update

--- public/welcome.js
+++ public/welcome.js
@@ -2,6 +2,7 @@ function start(){
   joinGame(document.getElementById('input').value);
   document.getElementById('welcome').style.display = "none";
   document.getElementById('display').style.display = "block";
+  joinGame = start = () => {};
 }
 document.addEventListener('keydown', (event) => {
   const keyName = event.key;
