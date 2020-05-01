3a70d6a6b04e0a311c0deb3cb0d00ac60c3a53fe
--global <--local>
2020-05-01 13:45:37 -0700

    removed check on self

--- index.js
+++ index.js
@@ -218,10 +218,9 @@ function Projectile(source, type, pID, angleOffset){
       let dists = [];
       for(let i = 0; i < seekingList.length; i ++){
         let p = source.room.plyr[seekingList[i]];
-        if(source.id == p.id) continue;
         dists.push({P: seekingList[i], d: Math.abs((p.x+p.xv*3) - this.x) + Math.abs((p.y+p.yv*3) - this.y)});
       }
-      this.target = source.room.plyr[dists.sort((a, b) => a.d - b.d)[0].P];
+      this.target = source.room.plyr[dists.sort((a, b) => a.d - b.d)[source.room.type === TDM ? 0 : 1].P];
     }
   }
   this.d = stats.wep[type].range[pID];
